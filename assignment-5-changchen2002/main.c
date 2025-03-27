cfa52a23994edbf5c3b080a2fdd46cb7e34cba88
changchen2002 <changchen2002@outlook.com>
2025-03-27 09:37:03 -0700

    lab9

--- assignment-5-changchen2002/main.c
+++ assignment-5-changchen2002/main.c
@@ -21,27 +21,93 @@ char getFirstChar(char *geneticString) {
 }
 
 Node* constructTree(char *geneticString) {
-   // TODO : Write your code here
+    if (geneticString[0] == '\0') {
+        return NULL;
+    }
+    
+    char firstChar = getFirstChar(geneticString);
+    if(firstChar == '\n') {
+        return NULL;
+    }
+
+    Node *root = createNode(firstChar);
+    root->left = constructTree(geneticString);
+    root->right = constructTree(geneticString);
+
+    return root;
 }
 
 void preorderTraversal(Node* root) {
-   // TODO : Write your code here
-
+    if (root == NULL) return;
+    printf("%c", root->data);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
 }
 
 void inorderTraversal(Node* root) {
-     // TODO : Write your code here
-
+    if (root == NULL) return;
+    inorderTraversal(root->left);
+    printf("%c", root->data);
+    inorderTraversal(root->right);
 }
 
 void postorderTraversal(Node* root) {
-     // TODO : Write your code here
+    // TODO : Write your code here
+    if (root == NULL) return;  
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    printf("%c", root->data);
+}
 
+typedef struct QueueNode {
+    Node* treeNode;
+    struct QueueNode* next;
+} QueueNode;
+
+typedef struct Queue {
+    QueueNode *front, *rear;
+} Queue;
+
+Queue* createQueue() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+void enqueue(Queue* q, Node* treeNode) {
+    QueueNode* newNode = (QueueNode*)malloc(sizeof(QueueNode));
+    newNode->treeNode = treeNode;
+    newNode->next = NULL;
+    if (q->rear == NULL) {
+        q->front = q->rear = newNode;
+        return;
+    }
+    q->rear->next = newNode;
+    q->rear = newNode;
+}
+
+Node* dequeue(Queue* q) {
+    if (q->front == NULL) return NULL;
+    QueueNode* temp = q->front;
+    Node* treeNode = temp->treeNode;
+    q->front = q->front->next;
+    if (q->front == NULL) q->rear = NULL;
+    free(temp);
+    return treeNode;
 }
 
 void breadthFirstTraversal(Node* root) {
-   // TODO : Write your code here
+    // TODO : Write your code here
+    Queue* q = createQueue();
+    enqueue(q, root);
 
+    while (q->front != NULL) {
+        Node* curr = dequeue(q);
+        printf("%c ", curr->data);
+
+        if (curr->left) enqueue(q, curr->left);
+        if (curr->right) enqueue(q, curr->right);
+    }
 }
 
 int main() {
